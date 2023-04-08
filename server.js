34f9e159587add989f6086b2eb94108edeba1c7d
Greg P <gpfeif20@gmail.com>
2023-04-08 10:38:52 -0400

    set up initial consts and middleware

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 
 
 
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const PORT = process.env.PORT || 3000
+const path = require('path')
+let data = require('./db/db.json')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static('public'))
+
+
